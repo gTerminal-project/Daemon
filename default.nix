5597755cba5db1d4101650585bea0aac95601d9f
Pascal Dietrich <80521473+Cameo007@users.noreply.github.com>
2023-09-10 11:42:58 +0200

    Create default.nix
    Added nix-shell configuration for nixOS.
    
    Signed-off-by: Pascal Dietrich <80521473+Cameo007@users.noreply.github.com>

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ unstable.rustc unstable.cargo unstable.gcc unstable.rustfmt unstable.clippy libiconv];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  NIX_ENFORCE_PURITY = 0;
+  shellHook = ''
+    codium .
+  '';
+}
